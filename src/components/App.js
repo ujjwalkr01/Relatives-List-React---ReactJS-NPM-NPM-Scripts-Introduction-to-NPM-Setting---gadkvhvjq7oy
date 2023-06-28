06967c373de21d7252c371ca5e94c63070ef7bdc
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-06-28 14:29:10 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol key={`relativeList`}>
+          <li key={`relativeListItem1`}>Ankita</li>
+          <li key={`relativeListItem2`}>Ujjwal</li>
+          <li key={`relativeListItem3`}>Ritika</li>
+          <li key={`relativeListItem4`}>Shubham</li>
+          <li key={`relativeListItem5`}>Aman</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
